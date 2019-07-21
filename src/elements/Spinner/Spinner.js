ca5225c5cc24b68d0a6bf2d70820086c5cedf994
Stefan Probst <stefanprobst@kolabnow.com>
2019-07-21 23:36:31 +0200

    Add Spinner

--- src/elements/Spinner/Spinner.js
+++ src/elements/Spinner/Spinner.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import clsx from 'clsx'
 
+import Loader from 'react-loader-spinner'
+
 import styles from './Spinner.module.css'
 
 const Spinner = ({ className }) => (
-  <div className={clsx(styles.spinner, className)}>Loading...</div>
+  <div className={clsx(styles.spinner, className)}>
+    <Loader type="Oval" height="40" width="40" color="var(--color-primary)" />
+  </div>
 )
 
 export default Spinner
