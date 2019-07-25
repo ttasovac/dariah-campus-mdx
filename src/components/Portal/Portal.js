2119eeeb9acfd5ab9ff03c2e0aeabadb3b8bb92f
Stefan Probst <stefanprobst@kolabnow.com>
2019-07-25 11:17:28 +0200

    Make netlify SSR happy

--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom'
 
-const Portal = ({ children }) =>
-  ReactDOM.createPortal(children, document.getElementById('overlay'))
+const Portal = ({ children }) => {
+  if (typeof document !== 'undefined') {
+    return ReactDOM.createPortal(children, document.getElementById('overlay'))
+  }
+  return null
+}
 
 export default Portal
