7416281e6f6dd1bed1cdd1c5d6c78a7af4ee6653
Stefan Probst <stefanprobst@kolabnow.com>
2019-07-25 12:50:45 +0200

    Cosmetics

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,33 @@
 import React from 'react'
 import clsx from 'clsx'
 
+// import { findPostsByTitle } from 'utils/find-posts-by-title'
+
 import styles from './SearchBar.module.css'
 
-const SearchBar = ({ className }) => (
-  <form
-    className={clsx(styles.searchBar, className)}
-    onSubmit={event => {
-      event.preventDefault()
-    }}
-  >
-    <label className={styles.searchBarLabel} htmlFor="searchbar">
-      <input
-        className={styles.searchBarInput}
-        id="searchbar"
-        placeholder="Search..."
-        type="search"
-      />
-    </label>
-  </form>
-)
+const SearchBar = ({ className }) => {
+  // const [searchTerm, setSearchTerm] = React.useState('')
+
+  // const searchResults = findPostsByTitle(searchTerm)
+
+  return (
+    <form
+      className={clsx(styles.searchBar, className)}
+      onSubmit={event => {
+        event.preventDefault()
+        // setSearchTerm(event.target.value)
+      }}
+    >
+      <label className={styles.searchBarLabel} htmlFor="searchbar">
+        <input
+          className={styles.searchBarInput}
+          id="searchbar"
+          placeholder="Search..."
+          type="search"
+        />
+      </label>
+    </form>
+  )
+}
 
 export default SearchBar
