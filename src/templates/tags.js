d011141643374be4c6dd45fbac01b8296405310b
Stefan Probst <stefanprobst@kolabnow.com>
2019-08-07 15:59:51 +0200

    Make /topics page show hero+tagcloud instead of tag cards

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,58 +1,24 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Head from 'components/Head/Head'
-import Pagination from 'components/Pagination/Pagination'
-import TagPreview from 'components/TagPreview/TagPreview'
+import Hero from 'components/Hero/Hero'
+import TagCloud from 'components/TagCloud/TagCloud'
 
-import Grid from 'elements/Grid/Grid'
-import LeadIn from 'elements/LeadIn/LeadIn'
 import Page from 'elements/Page/Page'
-import Title from 'elements/Title/Title'
 
-const TagsTemplate = ({ data }) => (
+import StudyImage from 'images/hero/study.svg'
+
+const TagsTemplate = () => (
   <Page>
     <Head title="Topics" />
-    <Title>Interested in particular topics?</Title>
-    <LeadIn>Browse learning resources based on their topic</LeadIn>
-    <Grid>
-      {data.tags.nodes.map((tag, i, tags) => (
-        <TagPreview
-          key={tag.id}
-          {...tag}
-          previous={tags[i - 1]}
-          next={tags[i + 1]}
-        />
-      ))}
-    </Grid>
-    <Pagination path="tags" {...data.tags.pageInfo} />
+    <Hero
+      title="Interested in particular topics?"
+      subtitle="Browse learning resources based on their topic"
+      image={StudyImage}
+    />
+
+    <TagCloud />
   </Page>
 )
 
 export default TagsTemplate
-
-export const query = graphql`
-  query($skip: Int!, $limit: Int!) {
-    tags: allTag(
-      limit: $limit
-      skip: $skip
-      sort: { fields: [name], order: [ASC] }
-    ) {
-      nodes {
-        description
-        id
-        name
-        posts {
-          id
-        }
-        slug
-      }
-      pageInfo {
-        currentPage
-        hasNextPage
-        hasPreviousPage
-        pageCount
-      }
-    }
-  }
-`
